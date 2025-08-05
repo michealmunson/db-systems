50fe141ca19c2476b1f471f45b9a6337f0973522
Micheal Munson <michealmunson@Micheals-Laptop.local>
2025-08-04 22:06:47 -0700

    checking memory allocation for data types

--- my-first-program.cpp
+++ my-first-program.cpp
@@ -2,6 +2,15 @@
 using namespace std;
 
 int main() {
-  cout << "Hello World!";
+  int myInt;
+  float myFloat;
+  double myDouble;
+  char myChar;
+
+  cout << sizeof(myInt) << " bytes" << "\n"; // Int are usually stored at 4 bytes in RAM
+  cout << sizeof(myFloat) << " bytes" << "\n"; // Floats are usually stored at 4 bytes in RAM
+  cout << sizeof(myDouble) << " bytes" << "\n"; // Doubles are usually stored at 8 bytes in RAM
+  cout << sizeof(myChar) << " bytes" << "\n"; // Chars are usually stored at 1 byte in RAM
+
   return 0;
 }
